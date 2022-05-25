26b203b5096dbfcee9e4cf13f6809c0483dff48b
Jenna <itcodegirl@gmail.com>
2022-05-25 14:53:50 -0500

    Register Update

--- src/Register.js
+++ src/Register.js
@@ -1,42 +1,115 @@
 import { useRef, useState, useEffect } from "react";
-import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from './api/axios';
+import axios from "./api/axios";
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
-
 const Register = () => {
-    const userRef = useRef();
-    // when the component loads
-    const errRef = useRef();
+  const userRef = useRef();
+  // when the component loads
+  const errRef = useRef();
 
-    const [user, setUser] = useState('');
-    const [validName, setValidName] = useState(false);
-    const [userFocus, setUserFocus] = useState(false);
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
 
-    const [pwd, setPwd] = useState('');
-    const [validPwd, setValidPwd] = useState(false);
-    const [pwdFocus, setPwdFocus] = useState(false);
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
 
-    const [matchPwd, setMatchPwd] = useState('');
-    const [validMatch, setValidMatch] = useState(false);
-    const [matchFocus, setMatchFocus] = useState(false);
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
 
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
 
-    //focus when the component loads
+  //focus when the component loads
 
-    useEffect(() => {
-        userRef.current.focus();
-    }, []);
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
 
+  useEffect(() => {
+    setValidName(USER_REGEX.test(user));
+    console.log(result);
+    console.log(user);
+    console.log(result);
+  }, [user]);
 
-    return (
-        <div></div>
-    )
-    
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(pwd));
+    console.log(result);
+    console.log(pwd);
+    console.log(result);
+    setValidMatch(pwd === matchPwd);
+  }, [pwd, matchPwd]);
 
-}
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd, matchPwd]);
+
+  return (
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      {/* this will annouce an error to a screen reader */}
+      <h1>Register</h1>
+      <form>
+        <label htmlFor="username">
+          Username:
+          <FontAwesomeIcon
+            icon={faCheck}
+            className={validName ? "valid" : "hide"}
+          />
+          <FontAwesomeIcon
+            icon={faTimes}
+            className={validName || !user ? "hide" : "invalid"}
+          />
+        </label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          autoComplete="off"
+          //we do not want to see previous values suggested
+          onChange={(e) => setUser(e.target.value)}
+          //ties the input to the user state
+          value={user}
+          required
+          // screen reader functions below
+          aria-invalid={validName ? "false" : "true"}
+          aria-describedby="uidnote"
+          onFocus={() => setUserFocus(true)}
+          onBlur={() => setUserFocus(false)}
+        />
+        <p
+          id="uidnote"
+          className={
+            userFocus && user && !validName ? "instructions" : "offscreen"
+          }
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+          4 to 24 characters.
+          <br />
+          Must begin with a letter.
+          <br />
+          Letters, numbers, underscores, hyphens allowed.
+        </p>
+      </form>
+    </section>
+  );
+};
+
+export default Register;
